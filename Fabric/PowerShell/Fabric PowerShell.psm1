d630160d179be9e6a7023d5a51c22e20ef29c5df
Bradley Schacht <56415272+bradleyschacht@users.noreply.github.com>
2024-10-13 18:24:13 -0400

    Update and rename Fabric.psm1 to Fabric PowerShell.psm1

--- Fabric/PowerShell/Fabric PowerShell.psm1
+++ Fabric/PowerShell/Fabric PowerShell.psm1
@@ -1,32 +1,3 @@
-<#
-$PathToScript = if ($PSScriptRoot) { 
-    # Console or vscode debug/run button/F5 temp console
-    $PSScriptRoot 
-}
-else {
-    if ($psISE) {Split-Path -Path $psISE.CurrentFile.FullPath}
-    else {
-        if ($profile -match "VScode") { 
-            # vscode "Run Code Selection" button/F8 in integrated console
-            Split-Path $psEditor.GetEditorContext().CurrentFile.Path 
-        }
-        else { 
-            Write-Output "unknown directory to set path variable. exiting script."
-            exit
-        } 
-    } 
-}
-
-# Change the location to the folder where the scripts are stored. 
-Set-Location $PathToScript
-
-foreach($Module in (Get-ChildItem -Path .\Functions | Where-Object {$_.Extension -eq ".ps1"})) {
-    . $Module.FullName
-    Export-ModuleMember -Function $Module.BaseName
-}
-#>
-
-
 $FunctionList = @{
     "Find-FabricSQLMessage"         = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Find-FabricSQLMessage.ps1"
     "Get-FabricAccessToken"         = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricAccessToken.ps1"
@@ -54,3 +25,32 @@ foreach($Key in $FunctionList.Keys) {
     
     Export-ModuleMember -Function $Key
 }
+
+<#
+# Code to run locally.
+    $PathToScript = if ($PSScriptRoot) { 
+        # Console or vscode debug/run button/F5 temp console
+        $PSScriptRoot 
+    }
+    else {
+        if ($psISE) {Split-Path -Path $psISE.CurrentFile.FullPath}
+        else {
+            if ($profile -match "VScode") { 
+                # vscode "Run Code Selection" button/F8 in integrated console
+                Split-Path $psEditor.GetEditorContext().CurrentFile.Path 
+            }
+            else { 
+                Write-Output "unknown directory to set path variable. exiting script."
+                exit
+            } 
+        } 
+    }
+    
+    # Change the location to the folder where the scripts are stored. 
+    Set-Location $PathToScript
+    
+    foreach($Module in (Get-ChildItem -Path .\Functions | Where-Object {$_.Extension -eq ".ps1"})) {
+        . $Module.FullName
+        Export-ModuleMember -Function $Module.BaseName
+    }
+#>
