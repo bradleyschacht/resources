c91f10e5502368fde2fdeab8c5b514bb780e5d4e
Bradley Schacht <bradley.schacht@gmail.com>
2024-10-13 18:02:22 -0400

    Update to powershell functions.

--- Fabric/PowerShell/Fabric Functions.ps1
+++ Fabric/PowerShell/Fabric Functions.ps1
@@ -1,7 +1,31 @@
+<#
+. {Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Load Fabric Functions.ps1")}
+#>
 
+$FunctionList = @{
+    "Find-FabricSQLMessage"         = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Find-FabricSQLMessage.ps1"
+    "Get-FabricAccessToken"         = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricAccessToken.ps1"
+    "Get-FabricCapacity"            = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricCapacity.ps1"
+    "Get-FabricCapacityMetrics"     = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricCapacityMetrics.ps1"
+    "Get-FabricCUPricePerHour"      = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricCUPricePerHour.ps1"
+    "Get-FabricItem"                = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricItem.ps1"
+    "Get-FabricLakehouse"           = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricLakehouse.ps1"
+    "Get-FabricSqlConnectionString" = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricSqlConnectionString.ps1"
+    "Get-FabricWarehouse"           = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricWarehouse.ps1"
+    "Get-FabricWorkspace"           = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricWorkspace.ps1"
+    "Invoke-FabricRestMethod"       = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Invoke-FabricRestMethod.ps1"
+    "Invoke-FabricSqlCommand"       = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Invoke-FabricSqlCommand.ps1"
+    "New-FabricWorkspace"           = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/New-FabricWorkspace.ps1"
+    "Remove-FabricWorkspace"        = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Remove-FabricWorkspace.ps1"
+    "Resume-FabricCapacity"         = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Resume-FabricCapacity.ps1"
+    "Set-FabricCapacitySku"         = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Set-FabricCapacitySku.ps1"
+    "Set-FabricWorkspaceCapacity"   = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Set-FabricWorkspaceCapacity.ps1"
+    "Suspend-FabricCapacity"        = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Suspend-FabricCapacity.ps1"
+    "Test-FabricId"                 = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Test-FabricId.ps1"
+}
 
-
-
-
-
-
+Clear-host
+foreach($Key in $FunctionList.Keys) {
+    . {Invoke-Expression (Invoke-WebRequest -Uri $FunctionList[$Key])}
+    "Loaded {0} from {1}" -f $Key, $FunctionList[$Key]
+}
--- Fabric/PowerShell/Fabric.psm1
+++ Fabric/PowerShell/Fabric.psm1
@@ -23,3 +23,37 @@ foreach($Module in (Get-ChildItem -Path .\Functions | Where-Object {$_.Extension
     . $Module.FullName
     Export-ModuleMember -Function $Module.BaseName
 }
+
+
+
+<#
+
+$FunctionList = @{
+    "Find-FabricSQLMessage"         = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Find-FabricSQLMessage.ps1"
+    "Get-FabricAccessToken"         = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricAccessToken.ps1"
+    "Get-FabricCapacity"            = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricCapacity.ps1"
+    "Get-FabricCapacityMetrics"     = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricCapacityMetrics.ps1"
+    "Get-FabricCUPricePerHour"      = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricCUPricePerHour.ps1"
+    "Get-FabricItem"                = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricItem.ps1"
+    "Get-FabricLakehouse"           = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricLakehouse.ps1"
+    "Get-FabricSqlConnectionString" = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricSqlConnectionString.ps1"
+    "Get-FabricWarehouse"           = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricWarehouse.ps1"
+    "Get-FabricWorkspace"           = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Get-FabricWorkspace.ps1"
+    "Invoke-FabricRestMethod"       = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Invoke-FabricRestMethod.ps1"
+    "Invoke-FabricSqlCommand"       = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Invoke-FabricSqlCommand.ps1"
+    "New-FabricWorkspace"           = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/New-FabricWorkspace.ps1"
+    "Remove-FabricWorkspace"        = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Remove-FabricWorkspace.ps1"
+    "Resume-FabricCapacity"         = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Resume-FabricCapacity.ps1"
+    "Set-FabricCapacitySku"         = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Set-FabricCapacitySku.ps1"
+    "Set-FabricWorkspaceCapacity"   = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Set-FabricWorkspaceCapacity.ps1"
+    "Suspend-FabricCapacity"        = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Suspend-FabricCapacity.ps1"
+    "Test-FabricId"                 = "https://raw.githubusercontent.com/bradleyschacht/resources/refs/heads/main/Fabric/PowerShell/Functions/Test-FabricId.ps1"
+}
+
+foreach($Key in $FunctionList.Keys) {
+    . {Invoke-Expression (Invoke-WebRequest -Uri $FunctionList[$Key])}
+    
+    Export-ModuleMember -Function $Key
+}
+
+#>
