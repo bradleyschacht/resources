3528760a4534cce7007caca6b1125665a5c196f4
Bradley Schacht <56415272+bradleyschacht@users.noreply.github.com>
2024-11-04 01:02:41 -0500

    Update code formatting Run Benchmark.ps1

--- Fabric/Benchmark/Framework/Run Benchmark.ps1
+++ Fabric/Benchmark/Framework/Run Benchmark.ps1
@@ -43,7 +43,7 @@ $BenchmarParameters = @{
 
     # Capacity Metrics
     CapacityMetricsWorkspace = ""
-	CapacityMetricsSemanticModelName = "Fabric Capacity Metrics"
+    CapacityMetricsSemanticModelName = "Fabric Capacity Metrics"
 
     # Runtime Variables
     GenerateNewScenarios                        = $true    <#  Default: $true  #>
